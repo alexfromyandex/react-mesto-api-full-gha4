79654bc002406ee6bc3766ba38f6866d4c90c2bc
Lyudmila Bedina <lyudmila.sergeevn@yandex.ru>
2023-12-23 19:03:51 +0300

    ПР15 первая попытка

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.get('/api/crash-test', () => {
   }, 0);
 });
 
-app.post('/api/signup', celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
@@ -43,7 +43,7 @@ app.post('/api/signup', celebrate({
   }),
 }), createUser);
 
-app.post('/api/signin', celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
